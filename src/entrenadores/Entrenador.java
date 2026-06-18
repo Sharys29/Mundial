345690919074fa98d01b0a2dfd4d31a14401bc55
jxsecitooo <joseandresalvarez1009@gmail.com>
2026-06-18 16:52:02 -0600

    Clase entrenador
    Se completo correctamente la clase entrenador

--- src/entrenadores/Entrenador.java
+++ src/entrenadores/Entrenador.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package entrenadores;
+import java.time.LocalDate;
 import personas.Persona;
 /**
  *
@@ -10,12 +11,18 @@ import personas.Persona;
  */
 public class Entrenador extends Persona{
     private int experiencia;
-    
+
+    public Entrenador(int experiencia,String nombre, String nacionalidad, LocalDate fechaNaci) {
+        super(nombre, nacionalidad, fechaNaci);
+        this.experiencia=experiencia;
+    }
+
     public int getExperiencia(){
         return this.experiencia;
     }
     
-    public int setExperiencia(int experiencia){
+    public void setExperiencia(int experiencia){
         this.experiencia=experiencia;
+    
     }
-}
+} 
