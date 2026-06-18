04cabc77c25746860943550904ce754c57d6b4c4
crmora <crmora@est.utn.ac.cr>
2026-06-18 14:59:00 -0600

    se crea los atributos para entrenador

--- src/Entranadores/entrenador.java
+++ src/Entranadores/entrenador.java
@@ -2,14 +2,14 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package jugadores;
+package Entranadores;
+
+import personas.Persona;
 
 /**
  *
- * @author sharys
+ * @author UTN
  */
-public class Jugador extends persona {
-    private int numero;
-    
+public class entrenador extends Persona {
     
 }
