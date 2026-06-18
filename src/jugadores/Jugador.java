1c04ac16a123c7da45f482d94030d31b652182d2
Sharys29 <sharysceciliano@gmail.com>
2026-06-18 15:04:26 -0600

    Modificacion Clase jugador
    Se agrego los atrbutos, setter, getter y constructor

--- src/jugadores/Jugador.java
+++ src/jugadores/Jugador.java
@@ -4,12 +4,42 @@
  */
 package jugadores;
 
+import java.time.LocalDate;
+import personas.Persona;
+
 /**
  *
  * @author sharys
  */
-public class Jugador extends persona {
+public class Jugador extends Persona {
     private int numero;
+    private String posicion; 
+    
+
+    public int getNumero() {
+        return numero;
+    }    
+
+    // MODO GETTER
+    public String getPosicion() {
+        return posicion;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public void setPosicion(String posicion) {
+        this.posicion = posicion;
+    }
+
+    public Jugador( String nombre,String nacionalidad, LocalDate fechaNaci, int numero, String posicion ) {
+        super(nombre, nacionalidad, fechaNaci);
+        this.numero = numero;
+        this.posicion = posicion;
+        
+    }
+   
     
     
 }
